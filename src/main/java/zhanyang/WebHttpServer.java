265065dd8465cf82b8480aa8abfb9ad1f7a3178c
clien9025 <302677599@qq.com>
2023-11-11 00:42:58 +0800

    Finish the task

--- src/main/java/zhanyang/WebHttpServer.java
+++ src/main/java/zhanyang/WebHttpServer.java
@@ -34,7 +34,7 @@ public class WebHttpServer {
         }
 
         // 从请求中提取文件路径
-        String requestedFile = requestLine.split("")[4];
+        String requestedFile = requestLine.split(" ")[1];
 
         // 如果请求是根目录，则默认返回 index.html
         if (requestedFile.equals("/")) {
@@ -57,7 +57,7 @@ public class WebHttpServer {
 
             // 发送 HTTP 头
             out.write(("HTTP/1.1 200 OK\r\n").getBytes());
-            out.write(("ContentType: " + contentType + "\r\n").getBytes());
+            out.write(("Content-Type: " + contentType + "\r\n").getBytes()); // 这里是修改后的代码
             out.write("\r\n".getBytes());
 
             // 发送文件内容
