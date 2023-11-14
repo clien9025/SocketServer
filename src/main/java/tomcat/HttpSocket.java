9508369e4737a0700038f4c0823c9525159e534c
clien9025 <302677599@qq.com>
2023-11-14 17:50:38 +0800

    增加异常判断

--- src/main/java/tomcat/HttpSocket.java
+++ src/main/java/tomcat/HttpSocket.java
@@ -2,6 +2,7 @@ package tomcat;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class HttpSocket {
 
@@ -11,24 +12,25 @@ public class HttpSocket {
          * ChatGPT 版本
          * */
 
-        // 确保web服务器正在这个端口上监听
-        Socket socket = new Socket("127.0.0.1", 8080);
-        OutputStream os = socket.getOutputStream();
-        boolean autoFlush = true;
-        PrintWriter out = new PrintWriter(os, autoFlush);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try {
+            // 确保web服务器正在这个端口上监听
+            Socket socket = new Socket("127.0.0.1", 8080);
+            OutputStream os = socket.getOutputStream();
+            boolean autoFlush = true;
+            PrintWriter out = new PrintWriter(os, autoFlush);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        // 发送一个 HTTP 请求到 web 服务器
-        out.println("GET /index.html HTTP/1.1");
-        out.println("Host: localhost:8080"); // 确保这与您的服务器监听的端口一致
-        out.println("Connection: close");
-        out.println();
+            // 发送一个 HTTP 请求到 web 服务器
+            out.println("GET /index.html HTTP/1.1");
+            out.println("Host: localhost:8080"); // 确保这与您的服务器监听的端口一致
+            out.println("Connection: close");
+            out.println();
 
-        /**
-         *这个逻辑存在问题，因为内部 while 循环设计有误。
-         *它应该在读取到 -1 时停止，但是代码中并没有在读取到 -1 之前跳出循环，这将导致无限循环。
-         *另外，当 in.read() 返回 -1 时，它会将 -1 作为一个字符添加到 StringBuffer 中，这是不正确的。
-         */
+            /**
+             *这个逻辑存在问题，因为内部 while 循环设计有误。
+             *它应该在读取到 -1 时停止，但是代码中并没有在读取到 -1 之前跳出循环，这将导致无限循环。
+             *另外，当 in.read() 返回 -1 时，它会将 -1 作为一个字符添加到 StringBuffer 中，这是不正确的。
+             */
 
 //        // read the response
 //        boolean loop = true;
@@ -46,18 +48,23 @@ public class HttpSocket {
 //            Thread.currentThread().sleep(50);
 //        }
 
-        // 读响应
-        StringBuilder sb = new StringBuilder(8096);
-        while (!in.ready()) {
-            Thread.sleep(50);
-        }
-        while (in.ready()) {
-            sb.append((char) in.read());
+            // 读响应
+            StringBuilder sb = new StringBuilder(8096);
+            while (!in.ready()) {
+                Thread.sleep(50);
+            }
+            while (in.ready()) {
+                sb.append((char) in.read());
+            }
+
+            // 在控制台显示响应
+            System.out.println(sb.toString());
+            socket.close();
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
-        // 在控制台显示响应
-        System.out.println(sb.toString());
-        socket.close();
     }
 
 
